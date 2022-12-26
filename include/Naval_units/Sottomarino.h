38f637a49dd4f1467188785d5636c94a60ce30d5
Magform <nicolas25.02.2003.nf@gmail.com>
2022-12-26 12:05:19 +0100

    Fatti .h di Naval_units

--- include/Naval_units/Sottomarino.h
+++ include/Naval_units/Sottomarino.h
@@ -1,1 +1,12 @@
- 
+#ifndef Sottomarino_H_
+#define Sottomarino_H_
+
+class Sottomarino : public Naval_units{
+    void set(std::string inizio, std::string fine);
+    void azione(std::string centro);
+};
+
+#include "../../lib/Naval_units/Sottomarino.hpp"
+
+#endif // Sottomarino_H_
+
--- include/Naval_units/Supporto.h
+++ include/Naval_units/Supporto.h
@@ -1,1 +1,11 @@
- 
+#ifndef Supporto_H_
+#define Supporto_H_
+
+class Supporto : public Naval_units{
+    void set(std::string inizio, std::string fine);
+    void azione(std::string centro);
+};
+
+#include "../../lib/Naval_units/Supporto.hpp"
+
+#endif // Supporto_H_
