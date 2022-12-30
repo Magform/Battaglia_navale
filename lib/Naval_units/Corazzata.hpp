6eee00a290f8af514151c4cea66629afb78541ca
Anna <118377483+QuarteA@users.noreply.github.com>
2022-12-30 15:11:50 +0100

    aggiustamento nel set

--- lib/Naval_units/Corazzata.hpp
+++ lib/Naval_units/Corazzata.hpp
@@ -5,46 +5,40 @@ class Corazzata : public NavalUnits{
 public:
     
     void set(std::string inizio, std::string fine){
-
+        
+        //converto in cordiate "matrici" la coordinata di inizio
         char cInizio=inizio.at(0);
         int xInizio=stoi(inizio.substr(1,2))-1;
         int yInizio;
-        if(cInizio<74){
+        if(cInizio<76){
             yInizio=cInizio-65;
-        }else{
-            yInizio=cInizio-67;
         }
-
+        
+        //converto in cordiate "matrici" la coordinata di fine
         char cFine=fine.at(0);
         int xFine=stoi(fine.substr(1,2))-1;
         int yFine;
-        if(cFine<74){
+        if(cFine<76){
             yFine=cFine-65;
-        }else{
-            yFine=cFine-67;
         }
         
 
-        if(xInizio==xFine){
+        if(xInizio==xFine){   //se hanno la stessa ascissa (la corazzata è situata in VERTICALE)
             for(int k=0; k<5; k++){
-                
-                yInizio=yInizio+1;
-                std::string Pos=yInizio+""+xInizio;
+                cInizio=cInizio+1;
+                std::string Pos=cInizio+""+xInizio;
                 Griglia.set("C", Pos);
             } 
         }else{
-            for(int k=0; k<5; k++){
-                
+            for(int k=0; k<5; k++){    //se hanno la stessa ordinata (la corazzata è situata in ORIZZONTALE)
                 xInizio=xInizio+1;
-                std::string Pos=yInizio+""+xInizio;
-
+                std::string Pos=cInizio+""+xInizio;
                 Griglia.set("C", Pos);
             }
         }
     }
     
     void azione(std::string centro, std::string obiettivo){
-
         char c=obiettivo.at(0);
         int XTarget=stoi(obiettivo.substr(1,2))-1;
         int YTarget;
@@ -53,7 +47,6 @@ public:
         }else{
             YTarget=c-67;
         }
-
         if(matrix[YTarget][XTarget]!=" "){         
 
             //mettere in lowercase il carattere colpito nella griglia di difesa avversaria
