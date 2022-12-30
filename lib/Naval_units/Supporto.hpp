3dda1d1a5a1c7b4fcfbfea59bc394b63d527330a
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2022-12-30 15:56:21 +0100

    Piccoli aggiustamenti

--- lib/Naval_units/Supporto.hpp
+++ lib/Naval_units/Supporto.hpp
@@ -1,7 +1,7 @@
 #include "../../include/Naval_units/Supporto.h"
 #include "../../lib/Naval_units/Naval_units.hpp"
 
-class Supporto : public NavalUnits{
+class Supporto : public Naval_units{
 public:
     
     void set(std::string inizio, std::string fine){
@@ -9,35 +9,27 @@ public:
         char cInizio=inizio.at(0);
         int xInizio=stoi(inizio.substr(1,2))-1;
         int yInizio;
-        if(cInizio<74){
+        if(cInizio<76){
             yInizio=cInizio-65;
-        }else{
-            yInizio=cInizio-67;
         }
 
         char cFine=fine.at(0);
         int xFine=stoi(fine.substr(1,2))-1;
         int yFine;
-        if(cFine<74){
+        if(cFine<76){
             yFine=cFine-65;
-        }else{
-            yFine=cFine-67;
         }
 
-        if(xInizio==xFine){
-            for(int k=0; k<3; k++){
-                
-                yInizio=yInizio+1;
-                std::string Pos=yInizio+""+xInizio;
-
-                Griglia.set("C", Pos);
+        if(xInizio==xFine){   //se hanno la stessa ascissa (la corazzata è situata in VERTICALE)
+             for(int k=0; k<3; k++){
+                cInizio=cInizio+1;
+                std::string Pos=cInizio+""+xInizio;
+                Griglia.set("S", Pos);
             } 
         }else{
-            for(int k=0; k<3; k++){
-                
+            for(int k=0; k<3; k++){    //se hanno la stessa ordinata (la corazzata è situata in ORIZZONTALE)
                 xInizio=xInizio+1;
-                std::string Pos=yInizio+""+xInizio;
-
+                std::string Pos=cInizio+""+xInizio;
                 Griglia.set("S", Pos);
             }
         }
@@ -45,18 +37,39 @@ public:
     
    void azione(std::string centro, std::string obiettivo){
 
-        char c=obiettivo.at(0);
+        char o=obiettivo.at(0);
         int XTarget=stoi(obiettivo.substr(1,2))-1;
         int YTarget;
-        if(c<74){
-            YTarget=c-65;
-        }else{
-            YTarget=c-67;
+        if(o<74){
+            YTarget=o-65;
         }
 
         if(isvalid(YTarget, XTarget)){
 
-            //elimino la vecchia posizione della nave di supporto e imposto quella nuova
+            Griglia.remove(centro);
+            
+            char c=centro.at(0);
+            int XCentro=stoi(centro.substr(1,2))-1;
+            int YCentro;
+            if(c<74){
+                YCentro=c-65;
+            }
+            
+            if((matrix[YCentro][XCentro+1]=="S")||(matrix[YCentro][XCentro+1]=="s")){
+                 std::string s1=YCentro+""+(XCentro+1);
+                 std::string s1=YCentro+""+(XCentro-1);
+                
+                 Griglia.remove(s1);
+                 Griglia.remove(s2);
+            
+            }else{
+                std::string s1=(c+1)+""+XCentro;
+                std::string s1=(c-1)+""+XCentro;
+                
+                Griglia.remove(s1);
+                Griglia.remove(s2);
+            }
+               
             //riparazione mettendo in maiuscolo tutte le lettere intorno
 
             }else{
@@ -76,6 +89,7 @@ public:
                 //Qui guardo se, oltre alla posizione data, sono liberi anche gli spazi laterali
             
             }else{
+                
                 if((matrix[YTarget+1][XTarget]==" ")&&((matrix[YTarget-1][XTarget]==" "))){ 
                  
                     return true;
