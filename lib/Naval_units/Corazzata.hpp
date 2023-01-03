6f3affe818f366e593a55ff14559b3e863c5e6a2
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-03 17:19:26 +0100

    Aggiunto dei controlli, ancora problemi con il chiamare le griglie

--- lib/Naval_units/Corazzata.hpp
+++ lib/Naval_units/Corazzata.hpp
@@ -7,41 +7,91 @@ public:
     void set(std::string inizio, std::string fine){
         
         //converto in cordiate "matrici" la coordinata di inizio
-        char cInizio=inizio.at(0);
+        cchar cInizio=inizio.at(0);
         int xInizio=stoi(inizio.substr(1,2))-1;
         int yInizio;
-        if(cInizio<76){
-            yInizio=cInizio-65;
+        if((cInizio>64)(cInizio<74)){
+            YInizio=cInizio-65;
+        }else{
+            throw std::invalid_argument("Carattere non valido");
         }
         
         //converto in cordiate "matrici" la coordinata di fine
         char cFine=fine.at(0);
         int xFine=stoi(fine.substr(1,2))-1;
         int yFine;
-        if(cFine<76){
-            yFine=cFine-65;
+        if((cFine>64)(cFine<74)){
+            YFine=cFine-65;
+        }else{
+            throw std::invalid_argument("Carattere non valido");
         }
         
 
-        if(xInizio==xFine){   //se hanno la stessa ascissa (la corazzata è situata in VERTICALE)
-             for(int k=0; k<5; k++){
-                cInizio=cInizio+1;
-                std::string Pos=cInizio+""+xInizio;
+        //Check per vedere se posso metterla in verticale
+
+        if(xInizio==xFine){
+            bool isValid=true;
+            for(int k=0; k<5; k++){      
+                std::string Pos=yInizio+""+xInizio;
+                if(Griglia.retrive(Pos)==" "){    //Se va fuori dalla griglia lancia l'eccezione da Griglia.hpp
+                    yInizio=yInizio+1;
+                }else{
+                    //Se un solo spazio è occupato, lancia una eccezione
+                    throw std::invalid_argument("Carattere non valido");
+                }
+            }
+
+            //Posizione orizzontale valida, si inserisce la lettera C nella griglia
+            yInizio=yInizio-5;
+            for(int k=0; k<5; k++){   
+                yInizio=yInizio+1;
+                std::string Pos=yInizio+""+xInizio;
                 Griglia.set("C", Pos);
             } 
+
+        
         }else{
-            for(int k=0; k<5; k++){    //se hanno la stessa ordinata (la corazzata è situata in ORIZZONTALE)
-                xInizio=xInizio+1;
-                std::string Pos=cInizio+""+xInizio;
-                Griglia.set("C", Pos);
+
+            //Check per vedere se posso metterla in orizzontale
+
+            if(yInizio==yFine){
+                bool isValid=true;
+                for(int k=0; k<5; k++){      
+                    std::string Pos=yInizio+""+xInizio;
+                    if(Griglia.retrive(Pos)==" "){    //Se va fuori dalla griglia lancia l'eccezione da Griglia.hpp
+                        xInizio=xInizio+1;
+                    }else{
+                        //Se un solo spazio è occupato, lancia una eccezione
+                        throw std::invalid_argument("Carattere non valido");
+                    }
+                }
+
+                //Posizione verticale valida, si inserisce la lettera C nella griglia
+                xInizio=xInizio-5;
+                for(int k=0; k<5; k++){
+                    
+                    xInizio=yInizio+1;
+                    std::string Pos=xInizio+""+xInizio;
+
+                    Griglia.set("C", Pos);
+                }
+            }else{
+
+                //Se non può essere messa nè in orizzontae nè in verticale, da errore
+                throw std::invalid_argument("Carattere non valido");
+
             }
         }
+    }
         //Sono Nicolas, ho buttato un occhi e cosi ho paura che non riesca a trovarti la griglia, volendo potresti aggiungere un terzo parametro ossia la griglia cosi che possiamo sceglierla.
         //Ti chiedo inoltre se riesci a fare in modo che se si prova a creare una barca dove ve ne é gia un altro oppure fuoria dalla griglia non crea nessuna casella ma lancia un eccezzione cosi poi la vado a cattuare per fare in modo che i bot impostino le navi in modo casuale.
         //PS. sarebbe in tutte le Naval_units da fare
     }
     
     void azione(std::string obiettivo){
+        
+        //Controllo se la corazzata è ancora in vita
+        if(!isAlive()) throw std::invalid_argument("Carattere non valido");
 
         if(Griglia.retrive(obiettivo)!=" "){         
 
@@ -51,6 +101,8 @@ public:
                 
             //Mette un "X" nella posizione data nella griglia di attacco 
             //(Non so in che griglia di difesa metterla)
+            
+            //Diminuire di 1 la vita della nave colpita
             
             //Check per vedere se la partita è finita
 
