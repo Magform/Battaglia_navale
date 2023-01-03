fd875c34239333e95f2689bf414935c1deb73b91
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-03 17:19:34 +0100

    Aggiunto dei controlli, ancora problemi con il chiamare le griglie

--- lib/Naval_units/Sottomarino.hpp
+++ lib/Naval_units/Sottomarino.hpp
@@ -18,37 +18,54 @@ public:
     }
     
    void azione(std::string obiettivo){
+       
+        //Controllo se il sottomarino è ancora in vita
+        if(!isAlive())  throw std::invalid_argument("Carattere non valido");
 
-        if(Griglia.retrive(obiettivo)==" "){
+        //Guardo se la posizione non è già occupata
+        if(Griglia.retrive(obiettivo)!=" ")     throw std::invalid_argument("Carattere non valido");
   
-            Griglia.remove(centro);
-            Griglia.set("E", obiettivo);
+        //rimuovo vecchia posizione e metto quella nuova
+        Griglia.remove(centro);
+        Griglia.set("E", obiettivo);
             
-            char c=obiettivo.at(0);
-            int XTarget=stoi(obiettivo.substr(1,2))-1;
-            int YTarget;
-            YTarget=c-65;
+        //Trasformo "obiettivo" in coordinate
+        char o=obiettivo.at(0);
+        int XTarget=stoi(obiettivo.substr(1,2))-1;
+        int YTarget;
+        if((o>64)(o<74)){
+            YTarget=o-65;
+        }else{
+             throw std::invalid_argument("Carattere non valido");
+        }
             
-            int YSearch=YTarget-2;
-            int XSearch=XTarget-2;
             
-            //inizio radar
-            for(int k=0; k<5; k++){
-                for(int j=0; j<5; j++){
-                    if(((YSearch+k)>0)&&((YSearch+k)<12)&&((XSearch+j)>0)&&((XSearch+j)<12))){   //if per vedere se si esce dalla matrice quando si fa la ricerca radar
-                        
-                        //Controllo se ci sono navi nel punto matrix[YSearch][XSearch]
-                        //e in caso metto una Y nella griglia di difesa
+        //inizio radar
+        YTarget=YTarget-2;
+        XTarget=XTarget-2;
+        int YSearch, XSearch;
+            
+        for(int Ycounter=0; Ycounter<5; Ycounter++){
+            YSearch=YTarget+Ycounter;
+
+            for(int Xcounter=0; Xcounter<5; Xcounter++){
+                
+                XSearch=YTarget+Ycounter;
+
+                //Guardo se la posizione dove sto cercando è all'interno della griglia
+                if((XSearch>0)&&(XSearch<12)&&(YSearch>0)&&(YSearch<12)){  
+
+                    std::string Pos=YSearch+""+XSearch;
+                    if(Griglia.retrive(Pos)!=" "){  //Qua bisognerebbe guardare nella griglia di difesa dell'avversario
+                            
+                           //Mette una Y nella posizione "Pos" della propria griglia d'attacco
                         
                     }
                 }
-            }
-            //fine del radar
+           }
+       }
+       
+       //fine del radar
             
-          }else{
-
-            throw std::invalid_argument("Carattere non valido");
-
-        }
     }
 };
