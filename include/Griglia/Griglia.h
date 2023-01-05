2700813dc5ea6dbb4dd3f8843aaabec6045806a1
Anna <118377483+QuarteA@users.noreply.github.com>
2023-01-05 15:07:40 +0100

    aggiunta di commenti

--- include/Griglia/Griglia.h
+++ include/Griglia/Griglia.h
@@ -5,15 +5,15 @@ using namespace srd;
 
 class Griglia{
 protected:
-    string matrix[12][12]; //matrice che rappresenta la griglia [y][x]
+    string matrix[12][12];      //matrice che rappresenta la griglia [y][x]
 public:
     Griglia();
 
     //funzioni utili
-    void set(string toSet, string location);
-    void clear();
-    void remove(string location);
-    string retrive(string location);
+    void set(string toSet, string location);    //setta "toSet" nalla posizione "location" 
+    void clear();                               //cancella TUTTA la griglia
+    void remove(string location);               //rimuove ciò che c'è nalla posizione "location"
+    string retrive(string location);            //restituisce la stringa nalla posizione "location"
 
     //overload operatori
     friend ostream& operator<<(ostream& os, const Griglia to_print);
