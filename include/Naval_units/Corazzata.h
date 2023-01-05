d62fa188ef3170e457d80bfd0df3d1320d605b88
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-05 22:23:38 +0100

    Lievi modifiche

--- include/Naval_units/Corazzata.h
+++ include/Naval_units/Corazzata.h
@@ -1,15 +1,15 @@
 #ifndef Corazzata_H_
 #define Corazzata_H_
 
+#include "../../lib/Naval_units/Corazzata.hpp"
+
 class Corazzata : public Naval_units{
 private:
     int vita = 5;
 public:
-    void set(string inizio, :string fine, Griglia g_difesa);
-    void azione(std::string obiettivo, Griglia g1_difesa, Griglia g1_attacco, Griglia g2_difesa);
+    void set(string inizio, string fine, Griglia& g_difesa);
+    void azione(string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa);
 
 };
-
-#include "../../lib/Naval_units/Corazzata.hpp"
 
 #endif // Corazzata_H_
