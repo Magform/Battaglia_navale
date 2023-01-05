3247ba1769760179dba6954fcdef71f0dea35891
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-05 22:31:18 +0100

    Lievi modifiche

--- include/Naval_units/Corazzata.h
+++ include/Naval_units/Corazzata.h
@@ -6,6 +6,9 @@
 class Corazzata : public Naval_units{
 private:
     int vita = 5;
+    string begin;
+    string end;
+    string centro;
 public:
     void set(string inizio, string fine, Griglia& g_difesa);
     void azione(string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa);
