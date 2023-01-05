722a16c09d87bd8b8a86e4107dde80b991a1f553
Anna <118377483+QuarteA@users.noreply.github.com>
2023-01-05 15:01:38 +0100

    Update Sottomarino.h

--- include/Naval_units/Sottomarino.h
+++ include/Naval_units/Sottomarino.h
@@ -5,7 +5,7 @@ using namespace std;
 
 class Sottomarino : public Naval_units{
 private:
-    int vita = 5;
+    int vita = 1;
 public:
     void set(string inizio, string fine, Griglia g_difesa);
     void azione(string obiettivo, Griglia g1_difesa, Griglia g1_attacco, Griglia g2_difesa);
