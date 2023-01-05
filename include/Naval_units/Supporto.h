cf95980a97712cff4ee0b2d99cca5f3c808a5f33
Anna <118377483+QuarteA@users.noreply.github.com>
2023-01-05 14:58:17 +0100

    Update Supporto.h

--- include/Naval_units/Supporto.h
+++ include/Naval_units/Supporto.h
@@ -1,14 +1,15 @@
 #ifndef Supporto_H_
 #define Supporto_H_
 
+using namespace std;
+
 class Supporto : public Naval_units{
 private:
     int vita = 5;
 public:
-    void set(string inizio, :string fine, Griglia g_difesa);
-    void azione(std::string obiettivo, Griglia g1_difesa, Griglia g1_attacco, Griglia g2_difesa);
+    void set(string inizio, string fine, Griglia g_difesa);
+    void azione(string obiettivo, Griglia g1_difesa, Griglia g1_attacco, Griglia g2_difesa);
 };
 
 #include "../../lib/Naval_units/Supporto.hpp"
-
 #endif // Supporto_H_
