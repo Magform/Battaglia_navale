64ecf6efc699586973949321b6d5bec308afe93d
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-08 17:46:41 +0100

    Sottomarino aggiornato

--- lib/Naval_units/Sottomarino.hpp
+++ lib/Naval_units/Sottomarino.hpp
@@ -1,23 +1,23 @@
 #include "../../include/Naval_units/Sottomarino.h"
-#include "../../lib/Naval_units/Naval_units.hpp"
+#include "../../include/Naval_units/Naval_units.h"
 
-class Sottomarino : public Naval_units{
-public:
+void Sottomarino::set(std::string inizio, std::string fine, Griglia& g_difesa){
+
+    if(inizio!=fine)    throw std::invalid_argument("Carattere non valido");
     
-    void set(std::string inizio, std::string fine){ 
-        
-        //Qua non ho capito se inizio e fine sono uguali, 
-        //essendo il Sottomarino di lunghezza 1
+    if(g_difesa.retrive(inizio)==" "){
 
-        if(Griglia.retrive(inizio)==" "){
-            Griglia.set("E", inizio);
-        }else{
-            throw std::invalid_argument("Carattere non valido");
-        }
+            g_difesa.set("E", inizio);
 
+    }else{
+
+        throw std::invalid_argument("Carattere non valido");
     }
-    
-   void azione(std::string obiettivo, Griglia g1_difesa, Griglia g1_attacco, Griglia g2_difesa){
+    centro=inizio;
+}
+
+
+void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa){
         //Il numero vicino alla g delle griglie rappresenta il giocatore (1 quello che sta compiendo l'azione)
         
        //Controllo se il sottomarino è ancora in vita
@@ -25,45 +25,52 @@ public:
 
         //Guardo se la posizione non è già occupata
         if(g1_difesa.retrive(obiettivo)!=" ")     throw std::invalid_argument("Carattere non valido");
-  
+        
+        //nuova posizione valida
         //rimuovo vecchia posizione e metto quella nuova
         g1_difesa.remove(centro);
         g1_difesa.set("E", obiettivo);
             
         //Trasformo "obiettivo" in coordinate
-        char o=obiettivo.at(0);
-        int XTarget=stoi(obiettivo.substr(1,2))-1;
+        char cTarget=obiettivo.at(0);
+        int XTarget=stoi(obiettivo.substr(1,2));
         int YTarget;
-        if((o>64)(o<74)){
-            YTarget=o-65;
+        if((cTarget<65)||(cTarget>78))  throw std::invalid_argument("Carattere non valido");
+        if(cTarget<74){
+            YTarget=cTarget-65;
         }else{
-             throw std::invalid_argument("Carattere non valido");
+            YTarget=cTarget-67;
         }
         
         //inizio radar
-        YTarget=YTarget-2;
-        XTarget=XTarget-2;
-        int YSearch, XSearch;
-            
+        int YSearch=YTarget-3;
+        int XSearch; 
+
         for(int Ycounter=0; Ycounter<5; Ycounter++){
-            YSearch=YTarget+Ycounter;
+            XSearch=XTarget-3;
+            YSearch=YSearch+1;
 
             for(int Xcounter=0; Xcounter<5; Xcounter++){
                 
-                XSearch=YTarget+Ycounter;
+                XSearch=XSearch+1;
 
                 //Guardo se la posizione dove sto cercando è all'interno della griglia
-                if((XSearch>0)&&(XSearch<12)&&(YSearch>0)&&(YSearch<12)){  
+                if((XSearch>=0)&&(XSearch<12)&&(YSearch>=0)&&(YSearch<12)){  
 
-                    std::string Pos=YSearch+""+XSearch;
-                    if(g2_difesa.retrive(Pos)!=" "){    //guardo nella griglia di difesa dell'avversario, se trovo qualcosa...
-                        g1_attacco.set("Y", Pos);	//...metto una Y nella posizione "Pos" nella mia griglia di attacco
+                    char cY=YSearch+65;
+                    std::string Pos(1,cY);
+                    Pos=Pos+to_string(XSearch);
+
+                    //if per vedere se nella posizione dove stiamo cercando cercando c'è una nave nemica
+                    if(g2_difesa.retrive(Pos)!=" "){ 
+                            
+                           //Nave trovata, metto una Y nella griglia d'attacco
+                            g1_attacco.set("Y", Pos);
                     }
                 }
            }
+       //fine del radar           
        }
-       
-       //fine del radar
-            
-    }
+
+    //Fine sottomarino        
 };
