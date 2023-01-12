b041293c5c7c404374f661fa0a8641cd875c302c
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-12 19:37:17 +0100

    Aggiustamenti della corazzata

--- lib/Naval_units/Corazzata.hpp
+++ lib/Naval_units/Corazzata.hpp
@@ -1,31 +1,27 @@
 #include "../../include/Naval_units/Corazzata.h"
-#include "../../include/Griglia/Griglia.h"
+#include <cstring>
 
 void Corazzata::set(std::string inizio, std::string fine, Griglia& g_difesa){
-
+       
     //converto in cordiate "matrici" la coordinata di inizio
     char cInizio=inizio.at(0);
-    int xInizio=stoi(inizio.substr(1,2));
+    int xInizio=stoi(inizio.substr(1,inizio.length()-1));
     int yInizio;
     if((cInizio<65)||(cInizio>78))  throw std::invalid_argument("Carattere non valido");
-    if(cInizio<74){
-        yInizio=cInizio-65;
-    }else{
-         yInizio=cInizio-67;
-    }
-
-
+    
+    yInizio=cInizio-65;
+    
+        
+        
     //converto in cordiate "matrici" la coordinata di fine
     char cFine=fine.at(0);
-    int xFine=stoi(fine.substr(1,2));
+    int xFine=stoi(fine.substr(1,fine.length()-1));
     int yFine;
     if((cFine<65)||(cFine>78))  throw std::invalid_argument("Carattere non valido");
-    if(cFine<74){
-        yFine=cFine-65;
-    }else{
-         yFine=cFine-67;
-    }
     
+    yFine=cFine-65;
+    
+
     //Metto in ordine le due coordinate
     if((yFine<yInizio)||(xFine<xInizio)){
         char scambioY=cFine;
@@ -34,79 +30,88 @@ void Corazzata::set(std::string inizio, std::string fine, Griglia& g_difesa){
 
         int scambioX=xFine;
         xFine=xInizio;
-        xInizio=xFine;
+        xInizio=scambioX;
     }
 
-    //Check per vedere se posso metterla in verticale
+    //Check per  vedere se posso metterla in verticale
 
     if(xInizio==xFine){
-
-        for(int k=0; k<5; k++){
+        char cSearch=cInizio;
+        for(int k=0; k<5; k++){   
 
             //Ad ogni ciclo cerco nella posizione Pos e mi sposto in giù
-            std::string Pos(1,cInizio);
+            if(cSearch==74||cSearch==75) cSearch=cSearch+2;
+            std::string Pos(1,cSearch);
             Pos=Pos+to_string(xInizio);
-
             if(g_difesa.retrive(Pos)==" "){    //Se va fuori dalla griglia lancia l'eccezione da Griglia.hpp
-                cInizio=cInizio+1;
-
+                cSearch=cSearch+1;
+                    
             }else{
                 //Se un solo spazio è occupato, lancia una eccezione
                 throw std::invalid_argument("Carattere non valido");
             }
         }
-
+            
         //Posizione verticale valida, si inserisce la lettera C nella griglia
-        cInizio=cInizio-5;
-
-        centro+=cInizio+2;
+        
+        
+        if(cInizio+2=='J'||cInizio+2=='K') {
+            centro=cInizio+4;
+        }else{
+            centro=cInizio+2;
+        }
         centro=centro+to_string(xInizio);
-
-        //for per inserire la corazzata in orizzontale nella griglia
-        for(int k=0; k<5; k++){
-
+            
+              
+            
+        for(int k=0; k<5; k++){   
+            if(cInizio==74||cInizio==75) cInizio=cInizio+2;                                    
             std::string Put(1,cInizio);
             Put=Put+to_string(xInizio);
 
             g_difesa.set("C", Put);
             cInizio=cInizio+1;
 
-        }
+        } 
 
-
+        
     }else{
 
         //Check per vedere se posso metterla in orizzontale
 
         if(yInizio==yFine){
-
-            for(int k=0; k<5; k++){
+               
+            for(int k=0; k<5; k++){      
+                if(cInizio==74||cInizio==75) cInizio=cInizio+2;
                 std::string Pos(1,cInizio);
                 Pos=Pos+to_string(xInizio);
 
                 if(g_difesa.retrive(Pos)==" "){    //Se va fuori dalla griglia lancia l'eccezione da Griglia.hpp
-                    xInizio=xInizio+1;
+                    xInizio=xInizio+1;                       
                 }else{
                     //Se un solo spazio è occupato, lancia una eccezione
-                    throw std::invalid_argument("Carattere non valido");
+                    throw std::invalid_argument("Carattere non valido"); 
                 }
             }
 
             //Posizione orizzontale valida, si inserisce la lettera C nella griglia
             xInizio=xInizio-5;
 
-            centro+=cInizio;
+            if(cInizio+2=='J'||cInizio+2=='K') {
+                centro=cInizio+4;
+            }else{
+                centro=cInizio+2;
+            }
             centro=centro+to_string(xInizio+2);
-
-            //for per inserire la corazzata in orizzontale nella griglia
+                
             for(int k=0; k<5; k++){
-
+                if(cInizio==74||cInizio==75) cInizio=cInizio+2;
                 std::string Put(1,cInizio);
                 Put=Put+to_string(xInizio);
 
                 g_difesa.set("C", Put);
                 xInizio=xInizio+1;
-
+            
             }
         }else{
 
@@ -115,40 +120,37 @@ void Corazzata::set(std::string inizio, std::string fine, Griglia& g_difesa){
 
         }
     }
-
-    //Imposto le variabili della nave
     begin=inizio;
-    end=fine;
-
+    end=fine;  
 }
 
-    void Corazzata::azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa){
-
+void Corazzata::azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa){
+        
     //Il numero vicino alla g delle griglie rappresenta il giocatore (1 quello che sta compiendo l'azione,
     //2 quello che subisce l'attacco)
 
     //if per vedere se nell'obiettivo c'è una nave non colpita
-    if((g2_difesa.retrive(obiettivo)=="C")||(g2_difesa.retrive(obiettivo)=="S")||(g2_difesa.retrive(obiettivo)=="E")){
+    if((g2_difesa.retrive(obiettivo)=="C")||(g2_difesa.retrive(obiettivo)=="S")||(g2_difesa.retrive(obiettivo)=="E")){         
 
         //nave colpita
-        string l=g2_difesa.retrive(obiettivo);
-        char lettera=l[0];
-        lettera=lettera+32;     //mette in minuscolo
-        string insert(1,lettera);
+        string lettera=g2_difesa.retrive(obiettivo);
+        char l=lettera[0];
+        l=tolower(l);
+        string insert(1,l);
         g2_difesa.set(insert, obiettivo);
 
         g1_attacco.set("X", obiettivo);     //Mette un "X" nella posizione data nella griglia di attacco
-
+            
         //Diminuire di 1 la vita della nave colpita
         //Check per vedere se la partita è finita
 
     }else{
-
+            
         //mancato
-
+            
         g1_attacco.set("O", obiettivo);     //Mette un "O" nella posizione data nella griglia di attacco
 
     }
-
-        //Fine azione della corazzata
+        
+    //Fine azione della corazzata
 };
