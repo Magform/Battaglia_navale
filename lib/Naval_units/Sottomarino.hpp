673ec67d87c96b817065bdb30ebdae4e32a7a3b4
Anna <118377483+QuarteA@users.noreply.github.com>
2023-01-12 21:42:20 +0100

    aggiunta commenti e sistemati gli errori

--- lib/Naval_units/Sottomarino.hpp
+++ lib/Naval_units/Sottomarino.hpp
@@ -1,16 +1,16 @@
 #include "../../include/Naval_units/Sottomarino.h"
 
 void Sottomarino::set(std::string inizio, std::string fine, Griglia& g_difesa){
-
-    if(inizio!=fine)    throw std::invalid_argument("Carattere non valido");
     
-    if(g_difesa.retrive(inizio)==" "){
+    if(inizio!=fine)    throw std::invalid_argument("Carattere non valido: le coordinate devono essere uguali");
+    
+    if(g_difesa.retrive(inizio)==" "){      //controllo che nel posto dove voglio inserire il sottomarino non sia già occupato
 
             g_difesa.set("E", inizio);
 
     }else{
 
-        throw std::invalid_argument("Carattere non valido");
+        throw std::invalid_argument("Casella gia' occupata");
     }
     centro=inizio;
 }
@@ -20,10 +20,10 @@ void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Grig
         //Il numero vicino alla g delle griglie rappresenta il giocatore (1 quello che sta compiendo l'azione)
         
        //Controllo se il sottomarino è ancora in vita
-        if(!isAlive())  throw std::invalid_argument("Carattere non valido");
+        if(!isAlive())  throw std::invalid_argument("Carattere non valido: il sottomarino e' stato affondato!");
 
         //Guardo se la posizione non è già occupata
-        if(g1_difesa.retrive(obiettivo)!=" ")     throw std::invalid_argument("Carattere non valido");
+        if(g1_difesa.retrive(obiettivo)!=" ")     throw std::invalid_argument("Casella gia' occupata");
         
         //nuova posizione valida
         //rimuovo vecchia posizione e metto quella nuova
@@ -38,8 +38,7 @@ void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Grig
         if((cTarget<65)||(cTarget>78))  throw std::invalid_argument("Carattere non valido");
         YTarget=cTarget-65;
        
-        
-        //inizio radar
+        //inizio RADAR
         int YSearch=YTarget-3;
         int XSearch; 
 
@@ -56,12 +55,11 @@ void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Grig
 
                     char cY=YSearch+65;
                     if(cY=='J'||cY=='K') cY+=2;
-                    
+
                     std::string Pos(1,cY);
                     Pos=Pos+to_string(XSearch);
 
                     try{
-                        
                         //if per vedere se nella posizione dove stiamo cercando cercando c'è una nave nemica
                         if(g2_difesa.retrive(Pos)!=" "){ 
                             
@@ -70,9 +68,6 @@ void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Grig
                         }
                     }catch(const std::invalid_argument ex){}
                 }
-           }
-       //fine del radar           
-       }
-
-    //Fine sottomarino        
-};
+           }           
+       } //fine RADAR
+}; //Fine sottomarino
