f871c35993d9629c2c1f315525418c4c29ed9a8e
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-14 11:41:04 +0100

    Lievi modifiche

--- include/Naval_units/Supporto.h
+++ include/Naval_units/Supporto.h
@@ -1,7 +1,7 @@
 #ifndef Supporto_H_
 #define Supporto_H_
 
-#include "../../lib/Naval_units/Supporto.hpp"
+#include "../../include/Naval_units/Naval_units.h"
 
 class Supporto : public Naval_units{
 private:
