2ba345dd759ac579626f741b608dec089e086de3
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-14 11:41:09 +0100

    Lievi modifiche

--- include/Naval_units/Corazzata.h
+++ include/Naval_units/Corazzata.h
@@ -1,7 +1,7 @@
 #ifndef Corazzata_H_
 #define Corazzata_H_
 
-#include "../../lib/Naval_units/Corazzata.hpp"
+#include "../../include/Naval_units/Naval_units.h"
 
 class Corazzata : public Naval_units{
 private:
