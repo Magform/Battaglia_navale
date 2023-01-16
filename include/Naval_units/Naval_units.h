a51e28072745042c74588bc0e28c82b40072a70d
Magform <nicolas25.02.2003.nf@gmail.com>
2023-01-16 14:08:02 +0100

    Update Naval_units.h

--- include/Naval_units/Naval_units.h
+++ include/Naval_units/Naval_units.h
@@ -12,7 +12,7 @@ protected:
     std::string centro;
 
 public:
-    bool isAlive(Griglia& g_difesa) { if (vita == 0) { retrun false; } return true; }
+    bool isAlive(Griglia& g_difesa) { if (vita == 0) { return false; } return true; }
     std::string get_centro() { return centro; }
     virtual void set(std::string inizio, std::string fine, Griglia& g_difesa) {}
     virtual void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa) {}
