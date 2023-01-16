28d6928638dd08ce917c38112fb32e524a4bea04
Magform <nicolas25.02.2003.nf@gmail.com>
2023-01-16 12:08:19 +0100

    Refuso

--- include/Naval_units/Sottomarino.h
+++ include/Naval_units/Sottomarino.h
@@ -13,7 +13,7 @@ private:
     std::string end;
     std::string centro;
 public:
-    string get_centro(); 
+    std::string get_centro(); 
     bool isAlive();
     void set(std::string inizio, std::string fine, Griglia& g_difesa);
     void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa);
