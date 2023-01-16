8c24aaa3a2f14a58ab339afd85db2fbf8631f162
Magform <nicolas25.02.2003.nf@gmail.com>
2023-01-16 18:39:21 +0100

    Update battaglia_navale.cpp

--- src/battaglia_navale.cpp
+++ src/battaglia_navale.cpp
@@ -29,7 +29,7 @@ int main() {
 	
 	battaglia.setup(start+1);
 
-	while (battaglia.winner()==0 && turni<=massimo_turni) {
+	while (turni<=massimo_turni) {
 		battaglia.command(start+1);
 		turni++;
 	}
