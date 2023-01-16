0d2e8e3ca44b37ac6e8f0a162a6d68b6ee8e6a8c
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-16 12:44:53 +0100

    Controlli vita nave

--- lib/Naval_units/Sottomarino.hpp
+++ lib/Naval_units/Sottomarino.hpp
@@ -1,49 +1,62 @@
 #include "Sottomarino.h"
+//#include "Naval_units.h"
 
 string Sottomarino::get_centro(){
     return centro;
 }
 
 void Sottomarino::set(std::string inizio, std::string fine, Griglia& g_difesa){
+
+    if(inizio!=fine)    throw std::invalid_argument("Carattere non valido");
     
-    if(inizio!=fine)    throw std::invalid_argument("Carattere non valido: le coordinate devono essere uguali");
-    
-    if(g_difesa.retrive(inizio)==" "){      //controllo che nel posto dove voglio inserire il sottomarino non sia già occupato
+    if(g_difesa.retrive(inizio)==" "){
 
             g_difesa.set("E", inizio);
 
     }else{
 
-        throw std::invalid_argument("Casella gia' occupata");
+        throw std::invalid_argument("Carattere non valido");
     }
     centro=inizio;
+
 }
 
-bool Sottomarino::isAlive(){ return vita!=0;}
 
-void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa){
-        //Il numero vicino alla g delle griglie rappresenta il giocatore (1 quello che sta compiendo l'azione)
-        
-       //Controllo se il sottomarino è ancora in vita
-        if(!isAlive())  throw std::invalid_argument("Carattere non valido: il sottomarino e' stato affondato!");
+
+bool Sottomarino::isAlive(Griglia& g_difesa){
+
+    if(vita==0) return false;
+    if(g_difesa.retrive(centro)=="e"){
+        g_difesa.remove(centro);
+        return false;
+    }
+    return true;
+}
+
+void Sottomarino::azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Griglia& g2_difesa){
+       
+        //Controllo se il sottomarino è ancora in vita
+        if(!isAlive(g1_difesa)) throw std::invalid_argument("Carattere non valido");
 
         //Guardo se la posizione non è già occupata
-        if(g1_difesa.retrive(obiettivo)!=" ")     throw std::invalid_argument("Casella gia' occupata");
-        
+        if(g1_difesa.retrive(obiettivo)!=" ")     throw std::invalid_argument("Carattere non valido");
+  
         //nuova posizione valida
         //rimuovo vecchia posizione e metto quella nuova
         g1_difesa.remove(centro);
         g1_difesa.set("E", obiettivo);
-        centro=obiettivo;  
-            
+        centro=obiettivo;   
         //Trasformo "obiettivo" in coordinate
         char cTarget=obiettivo.at(0);
         int XTarget=stoi(obiettivo.substr(1,obiettivo.length()-1));
         int YTarget;
         if((cTarget<65)||(cTarget>78))  throw std::invalid_argument("Carattere non valido");
-        YTarget=cTarget-65;
-       
-        //inizio RADAR
+        
+            YTarget=cTarget-65;
+        
+            
+   
+        //inizio radar
         int YSearch=YTarget-3;
         int XSearch; 
 
@@ -57,13 +70,10 @@ void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Grig
 
                 //Guardo se la posizione dove sto cercando è all'interno della griglia
                 if((XSearch>=0)&&(XSearch<12)&&(YSearch>=0)&&(YSearch<12)){  
-
                     char cY=YSearch+65;
                     if(cY=='J'||cY=='K') cY+=2;
-
                     std::string Pos(1,cY);
                     Pos=Pos+to_string(XSearch);
-
                     try{
                         //if per vedere se nella posizione dove stiamo cercando cercando c'è una nave nemica
                         if(g2_difesa.retrive(Pos)!=" "){ 
@@ -73,6 +83,9 @@ void azione(std::string obiettivo, Griglia& g1_difesa, Griglia& g1_attacco, Grig
                         }
                     }catch(const std::invalid_argument ex){}
                 }
-           }           
-       } //fine RADAR
-}; //Fine sottomarino
+           }
+       //fine del radar           
+       }
+
+    //Fine sottomarino        
+};
