d5aa457303e79957f8e9102d237cd9ed87b7415b
Magform <nicolas25.02.2003.nf@gmail.com>
2023-01-16 16:38:08 +0100

    Update replay.cpp

--- src/replay.cpp
+++ src/replay.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-int main(int argc, char** argv[]) {
+int main(int argc, char * argv[]) {
 	string loadtype{}, log_in{}, log_out{};
 	bool accepted = false;
 	int turni=0;
@@ -18,14 +18,21 @@ int main(int argc, char** argv[]) {
 
 	while (!accepted) {
 		try {
+			Replay battaglia("","","")";"
 			cout << "Inserire il tipo di replay che si desidera effettuare: " << endl;
 			cout << "v [nome_file_log] -> stampa a video il replay del file di log indicato" << endl;
 			cout << "f [nome_file_log] [nome_file_output_replay] -> scrive su file il replay del file di log indicato" << endl;
 			if (argv[0] == "v") {
-				Replay battaglia(argv[0], argv[1], "");
+				battaglia(argv[0], argv[1], "");
 			}
 			else if (argv[0] == "f") {
-				Replay battaglia(argv[0], argv[1], argv[2]);
+				battaglia(argv[0], argv[1], argv[2]);
+			}
+			else {
+				cout << "Argomento non valido: " << endl;
+				cout << " Inserire il tipo di replay che si desidera effettuare: " << endl;
+				cout << "v [nome_file_log] -> stampa a video il replay del file di log indicato" << endl;
+				cout << "f [nome_file_log] [nome_file_output_replay] -> scrive su file il replay del file di log indicato" << endl;
 			}
 			battaglia.setup();
 			while (battaglia.winner() == 0 && turni <= massimo_turni) {
