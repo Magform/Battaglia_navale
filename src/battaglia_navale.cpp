f7887be5b8c5963b187002dd2e47fdf0c1754802
Magform <nicolas25.02.2003.nf@gmail.com>
2023-01-18 09:01:18 +0100

    Minor editing

--- src/battaglia_navale.cpp
+++ src/battaglia_navale.cpp
@@ -11,34 +11,40 @@
 using namespace std;
 
 int main(int argc, char ** argv) {
+	int massimo_turni = 100;	//attenzioe se la si cambia qua bisonga cambiarla anche su replay
+	if (argc > 1) {
+		cout << "Sono stati inseriti troppi argomenti, solo il primo verra' considerator" << endl;
+	}
 	try {
-		int massimo_turni = 100;	//attenzioe se la si cambia qua bisonga cambiarla anche su replay
 		Naval_battle battaglia(argv[1], true);
-		int turni = 1;
-		srand(time(NULL));
-		int start = rand() % 2;  //si decide in maniera casuale chi inizia
-		if (start == 0) {
-			cout << "Il giocatore che inizia la partita è il \"giocatore 1\"" << "\n";
-		}
-		else {
-			cout << "Il giocatore che inizia la partita è il \"giocatore 2\"" << "\n";
-		}
-
-		battaglia.setup(start + 1);
-
-		while (battaglia.winner() == 0 && turni <= massimo_turni) {
-			battaglia.command(start + 1);
-			turni++;
-		}
-		cout << "Battaglia terminata" << endl;
-		if (turni = massimo_turni + 1 && battaglia.winner() == 0) {
-			cout << "Numero massimo di turni raggiunto, non vi e' un vincitore e la partita termina in pareggio" << endl;
-		}
-		else {
-			cout << "Il vincitore e' il giocatore " << battaglia.winner() << endl;
-		}
+	}
+	catch (const invalid_argument ex) {
+		cout << "Argomento non valido, inserire come argomento il tipo di partita ('pc' => giocatore vs computer; 'cc' => computer vs computer)" << endl;
 	}
 	catch (const logic_error ex) {
 		cout << "Inserire come argomento il tipo di partita ('pc' => giocatore vs computer; 'cc' => computer vs computer)" << endl;
+	}
+	int turni = 1;
+	srand(time(NULL));
+	int start = rand() % 2;  //si decide in maniera casuale chi inizia
+	if (start == 0) {
+		cout << "Il giocatore che inizia la partita è il \"giocatore 1\"" << "\n";
+	}
+	else {
+		cout << "Il giocatore che inizia la partita è il \"giocatore 2\"" << "\n";
+	}
+
+	battaglia.setup(start + 1);
+
+	while (battaglia.winner() == 0 && turni <= massimo_turni) {
+		battaglia.command(start + 1);
+		turni++;
+	}
+	cout << "Battaglia terminata" << endl;
+	if (turni = massimo_turni + 1 && battaglia.winner() == 0) {
+		cout << "Numero massimo di turni raggiunto, non vi e' un vincitore e la partita termina in pareggio" << endl;
+	}
+	else {
+		cout << "Il vincitore e' il giocatore " << battaglia.winner() << endl;
 	}
 }
