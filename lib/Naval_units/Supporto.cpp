147284b4e8bec91e0823897d817a708905b485e0
Calle-byte <118378147+Calle-byte@users.noreply.github.com>
2023-01-18 16:25:33 +0100

    Update Supporto.cpp

--- lib/Naval_units/Supporto.cpp
+++ lib/Naval_units/Supporto.cpp
@@ -43,7 +43,7 @@ void Supporto::set(std::string inizio, std::string fine,Griglia& g_difesa){
     //Check per vedere se posso metterla in verticale
 
     if(xInizio==xFine){
-        if((cFine>75)&&(cFine-cInzio!=4))  throw std::invalid_argument("Lunghezza nave non valida");
+        if((cFine>75)&&(cFine-cInizio!=4))  throw std::invalid_argument("Lunghezza nave non valida");
         if(cFine-cInizio!=2) throw std::invalid_argument("Lunghezza nave non valida");
         char cSearch=cInizio;
         for(int k=0; k<3; k++){   
